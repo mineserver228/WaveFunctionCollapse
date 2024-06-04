53f35dc07f2dc234f482b16b35ea8175d6afe204
Roma <romashkaviktorovich@gmail.com>
2024-06-04 13:01:52 +0300

    code that was preparing cells dict got moved from global level into the function loadCells

--- wfcGen.py
+++ wfcGen.py
@@ -2,83 +2,79 @@ import random
 
 from PIL import Image
 
+def loadCells(imageLink, tileSize=3, cropShift=False):
+    cropShift = cropShift
+    cells = {}
+    cropped = []
+    for ins in range(4):
+        print(ins/4)
+        field = Image.open(imageLink)
+        cropped.append([])
 
-res = 3
-cropShift = False
-cells = {}
-cropped = []
-for ins in range(4):
-    field = Image.open("image.png")
-    cropped.append([])
+        if cropShift:
+            field = field.rotate(90*ins)
 
-    if cropShift:
-        field = field.rotate(90*ins)
+            SX, SY = field.size
 
-        SX, SY = field.size
+            copy = Image.new("RGB", (SX*2, SY*2))
+            copy.paste(field, (0, 0))
+            copy.paste(field, (SX, 0))
+            copy.paste(field, (0, SY))
+            copy.paste(field, (SX, SY))
+            field = copy.copy()
 
-        copy = Image.new("RGB", (SX*2, SY*2))
-        copy.paste(field, (0, 0))
-        copy.paste(field, (SX, 0))
-        copy.paste(field, (0, SY))
-        copy.paste(field, (SX, SY))
-        field = copy.copy()
+            cropped[ins] = [[field.crop((x, y, x+tileSize, y+tileSize)) for x in range(SX)] for y in range(SY)]
 
-        cropped[ins] = [[field.crop((x, y, x+res, y+res)) for x in range(SX)] for y in range(SY)]
+        else:
+            field = field.rotate(90*ins)
+            SX = field.size[0]//tileSize*tileSize
+            SY = field.size[1]//tileSize*tileSize
+            field = field.crop((0, 0, SX, SY))
 
-    else:
-        field = field.rotate(90*ins)
-        SX = field.size[0]//res*res
-        SY = field.size[1]//res*res
-        field = field.crop((0, 0, SX, SY))
+            cropped[ins] = [[field.crop((x*tileSize, y*tileSize, x*tileSize+tileSize, y*tileSize+tileSize)) for x in range(SX//tileSize)] for y in range(SY//tileSize)]
 
-        cropped[ins] = [[field.crop((x*res, y*res, x*res+res, y*res+res)) for x in range(SX//res)] for y in range(SY//res)]
-
-    lcy = len(cropped[ins])
-    lcx = len(cropped[ins][0])
-
-    print(lcy, lcx)
+        lcy = len(cropped[ins])
+        lcx = len(cropped[ins][0])
 
 
+        # [left, top, right, bottom]uwu
+        # u will newer need that
 
-    # [left, top, right, bottom]uwu
-    # u will newer need that
+        for y in range(lcy):
+            for x in range(lcx):
+                flag = -1
+                for key in cells.keys():
+                    if cropped[ins][y][x] == cells[key]["img"]:
+                        flag = key
 
-    for y in range(lcy):
-        for x in range(lcx):
-            flag = -1
-            for key in cells.keys():
-                if cropped[ins][y][x] == cells[key]["img"]:
-                    flag = key
+                if flag != -1:
+                    cells[flag]["nbg"][0].append(cropped[ins][y][x-1-(tileSize-1)*int(cropShift)])
+                    cells[flag]["nbg"][1].append(cropped[ins][y-1-(tileSize-1)*int(cropShift)][x])
+                    cells[flag]["nbg"][2].append(cropped[ins][y][(x+1+(tileSize-1)*int(cropShift))%lcx])
+                    cells[flag]["nbg"][3].append(cropped[ins][(y+1+(tileSize-1)*int(cropShift))%lcy][x])
+                else:
+                    cells[len(cells.keys())] = {
+                        "nbg" : {
+                            0 : [cropped[ins][y][x-1-(tileSize-1)*int(cropShift)]],
+                            1 : [cropped[ins][y-1-(tileSize-1)*int(cropShift)][x]],
+                            2 : [cropped[ins][y][(x+1+(tileSize-1)*int(cropShift))%lcx]],
+                            3 : [cropped[ins][(y+1+(tileSize-1)*int(cropShift))%lcy][x]],
+                        }, 
+                        "img" : cropped[ins][y][x]
+                    }
 
-            if flag != -1:
-                cells[flag]["nbg"][0].append(cropped[ins][y][x-1-(res-1)*int(cropShift)])
-                cells[flag]["nbg"][1].append(cropped[ins][y-1-(res-1)*int(cropShift)][x])
-                cells[flag]["nbg"][2].append(cropped[ins][y][(x+1+(res-1)*int(cropShift))%lcx])
-                cells[flag]["nbg"][3].append(cropped[ins][(y+1+(res-1)*int(cropShift))%lcy][x])
-            else:
-                cells[str(len(cells.keys()))] = {
-                    "nbg" : {
-                        "0" : [cropped[ins][y][x-1-(res-1)*int(cropShift)]],
-                        "1" : [cropped[ins][y-1-(res-1)*int(cropShift)][x]],
-                        "2" : [cropped[ins][y][(x+1+(res-1)*int(cropShift))%lcx]],
-                        "3" : [cropped[ins][(y+1+(res-1)*int(cropShift))%lcy][x]],
-                    }, 
-                    "img" : cropped[ins][y][x]
-                }
-
-    for y in range(lcy):
-        for x in range(lcx):
-            for key in cells.keys():
-                if cells[key]["img"] == cropped[ins][y][x]:
-                    Gkey = key
-            for key in cells.keys():
-                for key2 in cells[key]["nbg"].keys():
-                    if key2 != "img":
-                        for i in range(len(cells[key]["nbg"][key2])):
-                            if cells[key]["nbg"][key2][i] == cropped[ins][y][x]:
-                                cells[key]["nbg"][key2][i] = Gkey
-
-    print(cells)
+        for y in range(lcy):
+            for x in range(lcx):
+                for key in cells.keys():
+                    if cells[key]["img"] == cropped[ins][y][x]:
+                        Gkey = key
+                for key in cells.keys():
+                    for key2 in cells[key]["nbg"].keys():
+                        if key2 != "img":
+                            for i in range(len(cells[key]["nbg"][key2])):
+                                if cells[key]["nbg"][key2][i] == cropped[ins][y][x]:
+                                    cells[key]["nbg"][key2][i] = Gkey
+    return cells
 
 def intersection(mass1, mass2):
     return list(set(mass1) & set(mass2))
@@ -93,7 +89,7 @@ class Cell:
         self.entropy = []
         self.checkCollapsedNeighbours = True
 
-    def collapse(self):
+    def collapse(self, cells):
 
         if len(self.entropy) > 0:
             self.collapsed = True
@@ -103,7 +99,7 @@ class Cell:
             self.nbg = pick["nbg"]
             self.img = pick["img"]
 
-    def setCell(self, idc):
+    def setCell(self, idc, cells):
         self.collapsed = True
         self.id = idc
         self.entropy = [idc]
@@ -134,14 +130,14 @@ class Cell:
                 if (self.gp[1]+xy)<len(grid):
                     if grid[self.gp[1]+xy][self.gp[0]].collapsed:
                         self.entropy = intersection(self.entropy, grid[self.gp[1]+xy][self.gp[0]].nbg[2 - xy])
-                if (self.gp[1]+xy)<len(grid):
+                if (self.gp[0]+xy)<len(grid):
                     if grid[self.gp[1]][self.gp[0]+xy].collapsed:
                         self.entropy = intersection(self.entropy, grid[self.gp[1]][self.gp[0]+xy].nbg[2 - xy])
 
         else:
             self.entropy = [self.id]
 
-    def getEntropy(self):
+    def getEntropy(self, cells):
         if self.collapsed:
             return -1
         else:
@@ -162,26 +158,26 @@ class Grid:
         self.backUpsLength = 20
         self.reloads = 0
 
-    def setRandom(self):
+    def setRandom(self, cells):
         self.grid = []
         for y in range(self.sy):
             self.grid.append([])
             for x in range(self.sx):
                 self.grid[y].append(Cell([x, y]))
-        self.grid[random.randint(0, self.sy-1)][random.randint(0, self.sx-1)].setCell(random.randint(0, len(cells)-1))
-        self.getFinalImage()
+        self.grid[random.randint(0, self.sy-1)][random.randint(0, self.sx-1)].setCell(random.randint(0, len(cells)-1), cells)
+        self.getFinalImage(cells)
 
-    def loadEntropy(self):
+    def loadEntropy(self, cells):
         self.entropySave = {}
         for y in range(0, self.sy):
             for x in range(0, self.sx):
                 if not self.grid[y][x].collapsed:
                     self.grid[y][x].loadEntropy(self.grid)
-                    entropy = self.grid[y][x].getEntropy()
+                    entropy = self.grid[y][x].getEntropy(cells)
                     self.entropySave[entropy] = self.entropySave.get(entropy, []) + [[x, y]]
 
-    def collapse(self):
-        self.loadEntropy()
+    def collapse(self, cells):
+        self.loadEntropy(cells)
         sortedEntropy = sorted(self.entropySave.keys())
         if len(sortedEntropy) == 0:
                 return True
@@ -192,8 +188,8 @@ class Grid:
         y = self.entropySave[sortedEntropy[0]][0][1]
         x = self.entropySave[sortedEntropy[0]][0][0]
 
-        self.grid[y][x].collapse()
-        self.loadEntropy()
+        self.grid[y][x].collapse(cells)
+        self.loadEntropy(cells)
         ZeroCheck = 0 in self.entropySave.keys()
         while 0 in self.entropySave.keys():
             if len(self.backUps) > 0:
@@ -201,8 +197,8 @@ class Grid:
                 self.grid[lb[1]][lb[0]] = Cell(lb)
                 self.backUps = self.backUps[:-1]
             else:
-                self.setRandom()
-            self.loadEntropy()
+                self.setRandom(cells)
+            self.loadEntropy(cells)
         if ZeroCheck:
             self.reloads += 2
             for j in range(self.reloads + random.randint(0, 4)):
@@ -211,35 +207,38 @@ class Grid:
                     self.grid[lb[1]][lb[0]] = Cell(lb)
                     self.backUps = self.backUps[:len(self.backUps)-1]
                 else:
-                    self.setRandom()
+                    self.setRandom(cells)
         else:
             self.reloads = 0
         self.backUps.append([x, y])
 
         if ITERATION % 400 == 0:
-            self.getFinalImage()
+            self.getFinalImage(cells)
         return False
 
 
-    def getFinalImage(self):
+    def getFinalImage(self, cells, tileSize=3):
         if self.GIMAGE is None:
-            self.GIMAGE = Image.new("RGB", (self.sx*res, self.sy*res))
+            self.GIMAGE = Image.new("RGB", (self.sx*tileSize, self.sy*tileSize))
         for y in range(self.sy):
             for x in range(self.sx):
                 if self.grid[y][x].id != -1:
-                    self.GIMAGE.paste(cells[self.grid[y][x].id]["img"], (x*res, y*res, x*res + res, y*res + res))
-        self.GIMAGE.save("result.png")
+                    self.GIMAGE.paste(cells[self.grid[y][x].id]["img"], (x*tileSize, y*tileSize, x*tileSize + tileSize, y*tileSize + tileSize))
+        self.GIMAGE.save("tileSizeult.png")
 
 
 if __name__ == "__main__":
+    
+    cells = loadCells("image.png", 3)
+
     grid = Grid(25, 25)
-    grid.setRandom()
-    grid.loadEntropy()
+    grid.setRandom(cells)
+    grid.loadEntropy(cells)
     ITERATION = 0
     flag = False
     while not flag:
         ITERATION += 1
         if ITERATION %400 == 0:
             print(ITERATION)
-        flag = grid.collapse()
-    grid.getFinalImage()
+        flag = grid.collapse(cells)
+    grid.getFinalImage(cells, 3)
