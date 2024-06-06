4abf99d98fe6d407b41032471cb685287c1d0f3d
Roma <romashkaviktorovich@gmail.com>
2024-06-06 12:16:31 +0300

    Grid class optimization
    - Deleted functions
      - loadEntropy - it was really laggy
    
    - Added functions
      - loadPosEntropy - loads entropy and saves it at pos in grid
      - loadEntropyAroundPos - loads entropy around pos
    
    - Changed functions
      - setRandom - now it loads entropy and not saving image
      - collapse - optimized and debugged, before it was just deleting image
        instead of making backups

--- wfcGen.py
+++ wfcGen.py
@@ -1,6 +1,9 @@
 import random
+import time
 
 from PIL import Image
+
+Gtimer = 0
 
 def loadCells(imageLink, tileSize=3, cropShift=False):
     cells = {}
@@ -164,24 +167,36 @@ class Grid:
 
     def setRandom(self):
         self.grid = []
+        self.entropySave = {}
         for y in range(self.sy):
             self.grid.append([])
             for x in range(self.sx):
                 self.grid[y].append(Cell([x, y]))
-        self.grid[random.randint(0, self.sy-1)][random.randint(0, self.sx-1)].setCell(random.randint(0, len(self.cells)-1), self.cells)
-        self.getFinalImage()
+        rand = [random.randint(0, self.sy-1), random.randint(0, self.sx-1)]
+        self.grid[rand[0]][rand[1]].setCell(random.randint(0, len(self.cells)-1), self.cells)
+        self.loadPosEntropy([rand[1], rand[0]])
+        self.loadEntropyAroundPos([rand[1], rand[0]])
 
-    def loadEntropy(self):
-        self.entropySave = {}
-        for y in range(0, self.sy):
-            for x in range(0, self.sx):
-                self.grid[y][x].loadEntropy(self.grid)
-                if not self.grid[y][x].collapsed:
-                    entropy = self.grid[y][x].getEntropy()
-                    self.entropySave[entropy] = self.entropySave.get(entropy, []) + [[x, y]]
+    def loadPosEntropy(self, pos):
+        for entropy, value in self.entropySave.items():
+            if [pos[0], pos[1]] in value:
+                self.entropySave[entropy].remove([pos[0], pos[1]])
+                if self.entropySave[entropy] == []:
+                    self.entropySave.pop(entropy, None)
+                break
+        self.grid[pos[1]][pos[0]].loadEntropy(self.grid)
+        entropy = self.grid[pos[1]][pos[0]].getEntropy()
+        self.entropySave[entropy] = self.entropySave.get(entropy, []) + [[pos[0], pos[1]]]
+    
+    def loadEntropyAroundPos(self, pos):
+        for x in [-1, 0, 1]:
+                for y in [-1, 0, 1]:
+                    if abs(x) == abs(y):
+                        continue
+                    if (pos[1]+y)<len(self.grid) and (pos[0]+x)<len(self.grid):
+                        self.loadPosEntropy([pos[0]+x, pos[1]+y])
 
     def collapse(self):
-        self.loadEntropy()
         sortedEntropy = sorted(self.entropySave.keys())
         if len(sortedEntropy) == 0:
                 return True
@@ -189,31 +204,26 @@ class Grid:
             sortedEntropy.remove(min(sortedEntropy))
             if len(sortedEntropy) == 0:
                 return True
-        y = self.entropySave[sortedEntropy[0]][0][1]
-        x = self.entropySave[sortedEntropy[0]][0][0]
+        entropyPos = random.choice(self.entropySave[sortedEntropy[0]])
+        y = entropyPos[1]
+        x = entropyPos[0]
 
         self.grid[y][x].collapse(self.cells)
-        self.loadEntropy()
-        ZeroCheck = 0 in self.entropySave.keys()
-        while 0 in self.entropySave.keys():
-            if len(self.backUps) > 0:
-                lb = self.backUps[len(self.backUps)-1]
-                self.grid[lb[1]][lb[0]] = Cell(lb)
-                self.backUps = self.backUps[:-1]
-            else:
-                self.setRandom()
-            self.loadEntropy()
-        if ZeroCheck:
+        self.loadPosEntropy([x, y])
+        self.loadEntropyAroundPos([x, y])
+        if 0 in self.entropySave:
             self.reloads += 2
-            for j in range(self.reloads + random.randint(0, 4)):
+            for i in range(self.reloads):
                 if len(self.backUps) > 0:
                     lb = self.backUps[len(self.backUps)-1]
                     self.grid[lb[1]][lb[0]] = Cell(lb)
-                    self.backUps = self.backUps[:len(self.backUps)-1]
+                    self.loadPosEntropy([lb[0], lb[1]])
+                    self.loadEntropyAroundPos([lb[0], lb[1]])
+                    self.backUps = self.backUps[:-1]
                 else:
                     self.setRandom()
         else:
-            self.reloads = 0
+            self.reloads -= 1
         self.backUps.append([x, y])
 
         if ITERATION % 400 == 0:
@@ -222,22 +232,21 @@ class Grid:
 
 
     def getFinalImage(self, saveFileName="result.png"):
-        if self.GIMAGE is None:
-            self.GIMAGE = Image.new("RGB", (self.sx*self.tileSize, self.sy*self.tileSize))
+        self.GIMAGE = Image.new("RGB", (self.sx*self.tileSize, self.sy*self.tileSize))
         for y in range(self.sy):
             for x in range(self.sx):
-                if self.grid[y][x].id != -1:
+                if self.grid[y][x].collapsed:
                     self.GIMAGE.paste(self.cells[self.grid[y][x].id]["img"], (x*self.tileSize, y*self.tileSize, x*self.tileSize + self.tileSize, y*self.tileSize + self.tileSize))
         self.GIMAGE.save(saveFileName)
+        
 
 
 if __name__ == "__main__":
     
-    cells = loadCells("image.png", 3, True)
+    cells = loadCells("image.png", 4, False)
 
-    grid = Grid(20, 20, cells, 3)
+    grid = Grid(100, 100, cells, 4)
     grid.setRandom()
-    grid.loadEntropy()
     ITERATION = 0
     flag = False
     while not flag:
